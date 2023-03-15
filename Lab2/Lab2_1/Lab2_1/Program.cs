c5a1438bc5380acf02f8cdfae35ecb53bf0db766
aleksandr <hersgory2134@gmail.com>
2023-03-15 22:38:48 +0300

    feat: add methods for lab 2.1

--- Lab2/Lab2_1/Lab2_1/Program.cs
+++ Lab2/Lab2_1/Lab2_1/Program.cs
@@ -1,37 +1,70 @@
-﻿
-// Разработайте программу, выполняющую считывание массива чисел с плавающей запятой, разделяемых
+﻿// Разработайте программу, выполняющую считывание массива чисел с плавающей запятой, разделяемых
 // пробелами, из стандартного потока ввода в vector, обрабатывающую его согласно заданию
 // Вашего варианта и выводящую в стандартный поток полученный массив (разделенный пробелами).
 // В программе должны быть выделены функции, выполняющие считывание массива, его обработку
 // и вывод результата.
 // Пустой массив, переданный программе – допустимые входные данные. При его обработке пустой массив должен оставаться пустым.
-
-using System.Numerics;
+using System.Globalization;
 
 class Program
 {
-    public static void Main()
+    public static int Main()
     {
-        var inputStream = Console.In;
-        var outputStream = Console.Out;
-        var numbersList = ParseCommandLine(inputStream);
+        try
+        {
+            var numbersList = ParseCommandLine();
+            var minimalElementInList = numbersList.Min();
+            var modifiedNumbersList = ModifyElementsByPredicate(numbersList, minimalElementInList);
+            PrintListToOutput(modifiedNumbersList);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return 1;
+        }
 
-
-        
-       
+        return 0;
     }
 
-
-
-    private static  List<float> ParseCommandLine(TextReader inputStream)
+    private static List<double> ParseCommandLine()
     {
-        var numbersList = new List<float>();
-        var t = inputStream.ReadToEnd();
-        Console.WriteLine(t);
-
-
-        
-
+        using var inputStream = Console.In;
+        var numbersList = new List<double>();
+        var readLine = Console.ReadLine();
+        while (readLine != null)
+        {
+            var elementsList = ParseStringToDoubleList(readLine);
+            numbersList.AddRange(elementsList);
+            readLine = inputStream.ReadLine();
+        }
+        inputStream.Close();
         return numbersList;
+    }
+
+    private static List<double> ParseStringToDoubleList(string inputString)
+    {
+        var stringElementList = inputString.Split(' ').ToList();
+        var numbersList = new List<double>();
+        stringElementList.ForEach(e =>
+        {
+            if (e != "")
+                numbersList.Add(double.Parse(e, CultureInfo.InvariantCulture.NumberFormat));
+        });
+        return numbersList;
+    }
+
+    private static List<double> ModifyElementsByPredicate(List<double> elementList, double minimalElement)
+    {
+        var modifiedElementList = new List<double>();
+        elementList.ForEach(e => modifiedElementList.Add(e * minimalElement));
+        return modifiedElementList;
+    }
+
+    private static void PrintListToOutput(List<double> modifiedNumbersList)
+    {
+        if (modifiedNumbersList.Count == 0)
+            Console.Out.Write("empty list");
+        modifiedNumbersList.ForEach(e => 
+            Console.Out.Write($"{Math.Round(e, 3)} "));
     }
 }
