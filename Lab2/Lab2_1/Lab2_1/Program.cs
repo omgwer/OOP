bc2771fa24175c60112f26eae396910b9e55580d
Kislitsyn <aleksandr.kislitsyn@travelline.ru>
2023-03-15 19:13:41 +0300

    feat: add lab 2-_1

---
+++ Lab2/Lab2_1/Lab2_1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// Разработайте программу, выполняющую считывание массива чисел с плавающей запятой, разделяемых
+// пробелами, из стандартного потока ввода в vector, обрабатывающую его согласно заданию
+// Вашего варианта и выводящую в стандартный поток полученный массив (разделенный пробелами).
+// В программе должны быть выделены функции, выполняющие считывание массива, его обработку
+// и вывод результата.
+// Пустой массив, переданный программе – допустимые входные данные. При его обработке пустой массив должен оставаться пустым.
+
+using System.Numerics;
+
+class Program
+{
+    public static void Main()
+    {
+        var inputStream = Console.In;
+        var outputStream = Console.Out;
+        var numbersList = ParseCommandLine(inputStream);
+
+
+        
+       
+    }
+
+
+
+    private static  List<float> ParseCommandLine(TextReader inputStream)
+    {
+        var numbersList = new List<float>();
+        var t = inputStream.ReadToEnd();
+        Console.WriteLine(t);
+
+
+        
+
+        return numbersList;
+    }
+}
