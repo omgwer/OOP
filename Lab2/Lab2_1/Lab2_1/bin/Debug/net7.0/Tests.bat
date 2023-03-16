1b674a8bc1582e6464e237f8c501231e8f7dfbd2
Kislitsyn <aleksandr.kislitsyn@travelline.ru>
2023-03-16 18:20:31 +0300

    fix

--- Lab2/Lab2_1/Lab2_1/bin/Debug/net7.0/Tests.bat
+++ Lab2/Lab2_1/Lab2_1/bin/Debug/net7.0/Tests.bat
@@ -1,25 +1,25 @@
 @rem 1 Невалидное значение 
-@call .\Lab2_1.exe < invalidFile1.txt  > out.txt
+@call .\Lab2_1.exe < invalidFile1.txt  > tmp.txt
 @if not %errorlevel% == 1 echo "Test 1 Fail"
 @if %errorlevel% == 1 echo "Test 1 Success"
 
 @rem 2 Позитивный кейс пустая строка
-@call .\Lab2_1.exe < emptyFile.txt > tmp.txt
-@fc .\tmp.txt .\emptyFile.txt
+@call .\Lab2_1.exe < emptyFile.txt > tmp.txt  
 @if not %errorlevel% == 0 echo "Test 2 Fail"
-@if %errorlevel% == 0 echo "Test 2 Success      
+@if %errorlevel% == 0 echo "Test 2 Success    
+@fc .\tmp.txt .\emptyFile.txt                   
                           
 @rem 3 Позитивный кейс преобразование одной строки
-@call .\Lab2_1.exe < inputFile1.txt > tmp.txt
-@fc .\tmp.txt .\ethalonFile1.txt              
+@call .\Lab2_1.exe < inputFile1.txt > tmp.txt   
 @if not %errorlevel% == 0 echo "Test 3 Fail"
-@if %errorlevel% == 0 echo "Test 3 Success"   
-      
+@if %errorlevel% == 0 echo "Test 3 Success" 
+@fc .\tmp.txt .\ethalonFile1.txt 
+
 @rem 4 Позитивный кейс преобразование нескольких строки
-@call .\Lab2_1.exe < inputFile2.txt > tmp.txt
-@fc .\tmp.txt .\ethalonFile2.txt              
+@call .\Lab2_1.exe < inputFile2.txt > tmp.txt    
 @if not %errorlevel% == 0 echo "Test 4 Fail"
-@if %errorlevel% == 0 echo "Test 4 Success"    
+@if %errorlevel% == 0 echo "Test 4 Success" 
+@fc .\tmp.txt .\ethalonFile2.txt                          
 
 @goto end                                
 
