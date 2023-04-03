0b3df9bd4872bcf5da0d8a040438281d411efb43
Alexandr Kislitsyn <63747048+omgwer@users.noreply.github.com>
2023-04-03 19:37:22 +0300

    fix

--- Lab2/Lab2_3/Lab2_3/Program.cs
+++ Lab2/Lab2_3/Lab2_3/Program.cs
@@ -9,21 +9,23 @@ var dictionary = new MiniDictionary();
 TextReader input = Console.In;
 TextWriter output = Console.Out;
 
-while (dictionary.IsRun)
-{
-   var word = Console.ReadLine();
-   var translatedWord = dictionary.Translate(word!);
-   if (translatedWord == null)
-   {
-      var response = dictionary.AskToUpdate(word);
-      if (response)
-         dictionary.Update(word);
-   }
-   else
-   {
-      Console.WriteLine(translatedWord);
-   }
-}
+// while (dictionary.IsRun)
+// {
+//    var word = Console.ReadLine();
+//    var translatedWord = dictionary.Translate(word!);
+//    if (translatedWord == null)
+//    {
+//       var response = dictionary.AskToUpdate(word);
+//       if (response)
+//          dictionary.Update(word);
+//    }
+//    else
+//    {
+//       Console.WriteLine(translatedWord);
+//    }
+// }
+
+Dictionary<string, List<string>>
 
 
 
