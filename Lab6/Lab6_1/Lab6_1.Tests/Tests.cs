2bae06a88d51a21e752219c3174247afbc2ddb6b
aleksandr <hersgory2134@gmail.com>
2023-04-30 16:10:52 +0300

    add tests project for lab 6.1

---
+++ Lab6/Lab6_1/Lab6_1.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Lab6_1.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
