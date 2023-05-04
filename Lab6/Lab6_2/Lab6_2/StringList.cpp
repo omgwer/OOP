6b66e748d50cae5b401eecdacf147d9d39251952
Kislitsyn <aleksandr.kislitsyn@travelline.ru>
2023-05-04 18:15:21 +0300

    feat: добавил мето добавления в конец

--- Lab6/Lab6_2/Lab6_2/StringList.cpp
+++ Lab6/Lab6_2/Lab6_2/StringList.cpp
@@ -9,4 +9,43 @@ StringList::StringList(const StringList& stringList)
 	}
 }
 
+StringList::StringList(StringList&& stringList)
+{
+	m_first = std::move(stringList.m_first);
+	m_last = std::move(stringList.m_last);
+	m_length = stringList.m_length;
+
+	stringList.m_first = nullptr;
+	stringList.m_last = nullptr;
+	stringList.m_length = 0;	
+}
+
+StringList::~StringList()
+{
+	Clear();
+	m_first = nullptr;
+	m_last = nullptr;
+	m_length = 0;
+}
+
+void StringList::PushFront(const std::string& value)
+{
+	if (m_first == nullptr)
+	{
+		m_first = std::make_unique<ListElement>();
+		m_first->value = value;
+		return;
+	}
+
+	if (m_last == nullptr) // значит элемент второй
+	{
+		m_last = std::make_unique<ListElement>();
+		m_last->value = value;
+		m_last->prev = std::move(m_first);
+		m_last->prev->next = std::move(m_last);
+	}
+
+	
+}
+
 
