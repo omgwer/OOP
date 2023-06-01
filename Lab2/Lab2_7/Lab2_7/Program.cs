987421e157af688d6beb6b716639089c71b717ec
Kislitsyn <aleksandr.kislitsyn@travelline.ru>
2023-06-01 15:36:13 +0300

    feat: init lab 2.7

---
+++ Lab2/Lab2_7/Lab2_7/Program.cs
@@ -0,0 +1,31 @@
+﻿
+class Program
+{
+    enum Operation
+    {
+        ADDICTION,      // +
+        MULTIPLICATION  // *
+    };
+    
+    public static int Main(string[] args)
+    {
+        string? expression = Console.ReadLine();
+        if (expression == null)
+        {
+            throw new IOException("Error in console read");
+        }
+
+        int result = EvaluateExpression(expression);
+        Console.WriteLine(result);
+
+
+        
+
+        return 0;
+    }
+    
+    private static int EvaluateExpression(string expression)
+    {
+        return 0;
+    }
+}
