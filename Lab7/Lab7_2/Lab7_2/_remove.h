c7c673c2174f8e83d01a490f6ea48e031512c78a
aleksandr <hersgory2134@gmail.com>
2023-06-06 22:30:42 +0300

    fix

--- Lab7/Lab7_2/Lab7_2/_remove.h
+++ Lab7/Lab7_2/Lab7_2/_remove.h
@@ -1,30 +1,62 @@
-﻿#pragma once
-
-#include <iostream>
-#include <new>
-
-template <typename T>
-struct Node
-{
-	Node() = default;
-	Node(const T& value) 
-	{
-		new (buffer) T(value);
-	}
-	T& Value() noexcept { return *reinterpret_cast<T*>(&buffer); }
-	void Destroy() noexcept { Value().~T(); }
-private:
-	alignas(T) char buffer[sizeof(T)];
-};
-
-int main(int argc, char* argv[])
-{
-	auto* node = new Node<std::string>("hello");
-	node->Value() = "hello1";
-	node->Destroy();
-	delete node;
-
-	auto sentinelNode = new Node<std::string>();
-	delete sentinelNode;
-
-}
+﻿//
+// template <typename T> struct ListElement
+// {
+// 	ListElement() = default;
+//
+// 	ListElement(const T& value, ListElement* prevPtr = nullptr, ListElement* nextPtr = nullptr)
+// 	{
+// 		new(buffer) T(value);
+// 		prev = prevPtr;
+// 		next = nextPtr;
+// 	}
+//
+// 	T& Value() noexcept { return *reinterpret_cast<T*>(&buffer); }
+// 	void Destroy() noexcept { Value().~T(); }
+//
+// 	ListElement* prev = nullptr;
+// 	ListElement* next = nullptr;
+//
+// private:
+// 	alignas(T) char buffer[sizeof(T)];
+// };
+//
+// namespace detail
+// {
+// template <typename T> class ListData
+// {
+// 	ListData()
+// 	{
+// 		m_root = new ListElement<T>();
+// 		m_root->next = m_root;
+// 		m_root->prev = m_root;
+// 	}
+// 	
+// 	ListData(ListData&& other) noexcept(false)
+// 	{
+// 		const auto newRootElement = new ListElement<T>();		
+// 		m_root = other.m_root;
+// 		m_length = other.m_length;
+// 		other.m_root = newRootElement;
+// 		other.m_root->next = newRootElement;
+// 		other.m_root->prev = newRootElement;
+// 		other.m_length = 0;
+// 	}
+// 	~ListData()
+// 	{
+// 		if (m_root->next == m_root)
+// 			return;	
+// 		auto currentNode = m_root->next;
+// 		while (currentNode != m_root)
+// 		{
+// 			const ListElement<T>* elementToDelete = currentNode;
+// 			currentNode = currentNode->next;
+// 			delete elementToDelete;
+// 		}
+// 		delete m_root;
+// 	}
+// public:	
+// 	ListElement<T>* m_root = nullptr;
+// 	size_t m_length = 0;	
+// };
+// }
+//
